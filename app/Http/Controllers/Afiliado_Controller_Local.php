8974dfdc27f569755cfdaead7f61b725c05ef7bc
fabianGerman <fabianontiveros_7@hotmail.com>
2024-03-20 08:40:48 -0300

    actualizado

--- app/Http/Controllers/Afiliado_Controller_Local.php
+++ app/Http/Controllers/Afiliado_Controller_Local.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\cobranzas\Afiliado;
-
+ 
 class Afiliado_Controller_Local extends Controller
 {
     public function list_afiliados(){
